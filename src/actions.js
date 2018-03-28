c81cace6be2429fc1437da590d5ebb229fdf4aa2
josephdlawson21 <josephdlawson21@gmail.com>
2018-03-28 12:01:53 -0400

    added heroku stuff

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 export function fetchState(stateCode) {
   return function(dispatch) {
     dispatch({type: 'LOADING'})
-    fetch("http://localhost:3000/state/" + stateCode)
+    fetch("https://national-parks-backend.herokuapp.com/state/" + stateCode)
       .then(res => res.json())
       .then(json => {
         dispatch({type: "FETCH_STATE", payload: json.data})
@@ -13,7 +13,7 @@ export function fetchState(stateCode) {
 export function fetchPark(parkCode) {
   return function(dispatch) {
     dispatch({type: 'LOADING'})
-    fetch("http://localhost:3000/parks/" + parkCode)
+    fetch("https://national-parks-backend.herokuapp.com/parks/" + parkCode)
       .then(res => res.json())
       .then(json => {
         dispatch({type: "FETCH_PARK", payload: json})
@@ -25,7 +25,7 @@ export function fetchPark(parkCode) {
 export function fetchFrontPage() {
   return function(dispatch) {
     dispatch({type: 'LOADING'})
-    fetch("http://localhost:3000/parks/frontPageArticles")
+    fetch("https://national-parks-backend.herokuapp.com/parks/frontPageArticles")
       .then(res => res.json())
       .then(json => {
         dispatch({ type: "FRONT_PAGE", payload: json.data });
@@ -37,7 +37,7 @@ export function fetchFrontPage() {
 export function searchForPark(query) {
   return function(dispatch) {
     dispatch({type: 'LOADING'})
-    fetch("http://localhost:3000/parks/search/" + query)
+    fetch("https://national-parks-backend.herokuapp.com/parks/search/" + query)
       .then(res => res.json())
       .then(json => {
         dispatch({ type: "SEARCH_RESULTS", payload: json.data });
