ec0460f8d558d33631abe54007f7b42b129781fc
Joseph Lawson <32317682+josephdlawson21@users.noreply.github.com>
2019-04-19 20:03:17 -0500

    fix

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,19 +3,18 @@
 import React from 'react'
 
 const Article = ({article, width}) => {
-  console.log('Article.jsx', article, article.listingImage, article.listingImage.url)
   return (
     <div className={`col s${width}`}>
       <div className="card">
         <div className="card-image waves-effect waves-block waves-light article-img">
-          <img className="activator" src={article.listingImage.url} alt={article.listingImage.altText}/>
+          <img className="activator" src={article.listingimage.url} alt={article.listingimage.altText}/>
         </div>
         <div className="card-content">
           <span className="card-title spanFix">{article.title}</span>
         </div>
         <div className="card-reveal">
           <span className="card-title grey-text text-darken-4">{article.title}<i className="material-icons right">close</i></span>
-          <p>{article.listingDescription}</p>
+          <p>{article.listingdescription}</p>
         </div>
       </div>
     </div>
